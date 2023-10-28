7d1d9af8f0acbc83cd472cf983b5908591dccd32
AOP <AOP-0@163.com>
2023-10-28 16:59:11 +0800

    Initial Commit

---
+++ Sources/SwiftMacroSE/SwiftMacroSE.swift
@@ -0,0 +1,15 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+/// A macro that produces both a value and a string containing the
+/// source code that generated the value. For example,
+///
+///     #stringify(x + y)
+///
+/// produces a tuple `(x + y, "x + y")`.
+@freestanding(expression)
+public macro stringify<T>(_ value: T) -> (T, String) = #externalMacro(module: "SwiftMacroSEMacros", type: "StringifyMacro")
+
+
+@attached(member)
+public macro InitTo() = #externalMacro(module: "SwiftMacroSEMacros", type: "InitTo")
